4b012f59eae29f73c2ed20d24222243e8ec15cc6
dEad.NOT <42716046+EHOTIK911@users.noreply.github.com>
2023-12-06 18:24:45 +0300

    Fix Ctrl+Z

--- CursorPaint/Cursor.cs
+++ CursorPaint/Cursor.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using Paint.model;
+using System.Xml.Linq;
 
 namespace Paint.CursorPaint
 {
@@ -41,6 +42,7 @@ namespace Paint.CursorPaint
                     Data = new PathGeometry() { Figures = { currentFigure } }
                 };
             MyCanvas.Children.Add(currentPath);
+            Canvas.SetZIndex(currentPath, int.MaxValue);
         }
         public void AddFigurePoint(Point point)
         {
